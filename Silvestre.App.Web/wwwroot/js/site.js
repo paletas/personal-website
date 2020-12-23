6e8ee15ce565fe3466cfbb139c8dc1dc03c9247f
João Silvestre <joaopaulosilvestre@hotmail.com>
2020-12-23 14:15:21 +0000

    Switched to TailwindCSS Launching a new homepage, with some details but not to many, main focus is to be able to send people towards WISC-3 tooling (WIP)

--- Silvestre.App.Web/wwwroot/js/site.js
+++ Silvestre.App.Web/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Navbar Toggle
+document.addEventListener('DOMContentLoaded', function () {
 
-// Write your JavaScript code.
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+
+                // Get the "main-nav" element
+                var $target = document.getElementById('main-nav');
+                var $targetButtonOpen = document.getElementById('main-nav-button-open');
+                var $targetButtonClosed = document.getElementById('main-nav-button-closed');
+
+                // Toggle the class on "main-nav"
+                $target.classList.toggle('hidden');
+                $targetButtonOpen.classList.toggle('hidden');
+                $targetButtonClosed.classList.toggle('hidden');
+            });
+        });
+    }
+
+});
